4fc5838cc55321bec353331fc01ad7f2038facd9
maizw <maizw@snovatech.cn>
2019-08-13 19:26:00 +0800

    集成zhiwei修改点： 1. 内核 打上 100ask补丁后代码

--- linux-2.6.22.6/linux-2.6.22.6/net/ipv6/netfilter/ip6t_HL.c
+++ linux-2.6.22.6/linux-2.6.22.6/net/ipv6/netfilter/ip6t_HL.c
@@ -1,102 +1,70 @@
-/*
- * Hop Limit modification target for ip6tables
- * Maciej Soltysiak <solt@dns.toxicfilms.tv>
- * Based on HW's TTL module
+/* Hop Limit matching module */
+
+/* (C) 2001-2002 Maciej Soltysiak <solt@dns.toxicfilms.tv>
+ * Based on HW's ttl module
  *
- * This software is distributed under the terms of GNU GPL
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
  */
 
+#include <linux/ipv6.h>
 #include <linux/module.h>
 #include <linux/skbuff.h>
-#include <linux/ip.h>
-#include <linux/ipv6.h>
 
+#include <linux/netfilter_ipv6/ip6t_hl.h>
 #include <linux/netfilter/x_tables.h>
-#include <linux/netfilter_ipv6/ip6t_HL.h>
 
 MODULE_AUTHOR("Maciej Soltysiak <solt@dns.toxicfilms.tv>");
-MODULE_DESCRIPTION("IP6 tables Hop Limit modification module");
+MODULE_DESCRIPTION("IP tables Hop Limit matching module");
 MODULE_LICENSE("GPL");
 
-static unsigned int ip6t_hl_target(struct sk_buff **pskb,
-				   const struct net_device *in,
-				   const struct net_device *out,
-				   unsigned int hooknum,
-				   const struct xt_target *target,
-				   const void *targinfo)
+static int match(const struct sk_buff *skb,
+		 const struct net_device *in, const struct net_device *out,
+		 const struct xt_match *match, const void *matchinfo,
+		 int offset, unsigned int protoff, int *hotdrop)
 {
-	struct ipv6hdr *ip6h;
-	const struct ip6t_HL_info *info = targinfo;
-	int new_hl;
-
-	if (!skb_make_writable(pskb, (*pskb)->len))
-		return NF_DROP;
-
-	ip6h = ipv6_hdr(*pskb);
+	const struct ip6t_hl_info *info = matchinfo;
+	const struct ipv6hdr *ip6h = ipv6_hdr(skb);
 
 	switch (info->mode) {
-		case IP6T_HL_SET:
-			new_hl = info->hop_limit;
+		case IP6T_HL_EQ:
+			return (ip6h->hop_limit == info->hop_limit);
 			break;
-		case IP6T_HL_INC:
-			new_hl = ip6h->hop_limit + info->hop_limit;
-			if (new_hl > 255)
-				new_hl = 255;
+		case IP6T_HL_NE:
+			return (!(ip6h->hop_limit == info->hop_limit));
 			break;
-		case IP6T_HL_DEC:
-			new_hl = ip6h->hop_limit - info->hop_limit;
-			if (new_hl < 0)
-				new_hl = 0;
+		case IP6T_HL_LT:
+			return (ip6h->hop_limit < info->hop_limit);
+			break;
+		case IP6T_HL_GT:
+			return (ip6h->hop_limit > info->hop_limit);
 			break;
 		default:
-			new_hl = ip6h->hop_limit;
-			break;
+			printk(KERN_WARNING "ip6t_hl: unknown mode %d\n",
+				info->mode);
+			return 0;
 	}
 
-	ip6h->hop_limit = new_hl;
-
-	return XT_CONTINUE;
+	return 0;
 }
 
-static int ip6t_hl_checkentry(const char *tablename,
-		const void *entry,
-		const struct xt_target *target,
-		void *targinfo,
-		unsigned int hook_mask)
-{
-	struct ip6t_HL_info *info = targinfo;
-
-	if (info->mode > IP6T_HL_MAXMODE) {
-		printk(KERN_WARNING "ip6t_HL: invalid or unknown Mode %u\n",
-			info->mode);
-		return 0;
-	}
-	if ((info->mode != IP6T_HL_SET) && (info->hop_limit == 0)) {
-		printk(KERN_WARNING "ip6t_HL: increment/decrement doesn't "
-			"make sense with value 0\n");
-		return 0;
-	}
-	return 1;
-}
-
-static struct xt_target ip6t_HL = {
-	.name 		= "HL",
+static struct xt_match hl_match = {
+	.name		= "hl",
 	.family		= AF_INET6,
-	.target		= ip6t_hl_target,
-	.targetsize	= sizeof(struct ip6t_HL_info),
-	.table		= "mangle",
-	.checkentry	= ip6t_hl_checkentry,
-	.me		= THIS_MODULE
+	.match		= match,
+	.matchsize	= sizeof(struct ip6t_hl_info),
+	.me		= THIS_MODULE,
 };
 
 static int __init ip6t_hl_init(void)
 {
-	return xt_register_target(&ip6t_HL);
+	return xt_register_match(&hl_match);
 }
 
 static void __exit ip6t_hl_fini(void)
 {
-	xt_unregister_target(&ip6t_HL);
+	xt_unregister_match(&hl_match);
 }
 
 module_init(ip6t_hl_init);
