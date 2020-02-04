051a78654d68be9061e290af0dbbb257172595d1
maizw <maizw@snovatech.cn>
2020-02-04 21:51:27 +0800

    集成zhiwei修改点: 1. my_bootloader/Makefile 根据韦东山的通用Makefile进行了调整

--- my_bootloader/config.mk
+++ my_bootloader/config.mk
@@ -1,45 +1,61 @@
+# 避免重复加载
+ifeq ($(CONFIG_MK),LIVE)
+
+else
+        CONFIG_MK = LIVE
+
+        
+		CROSS_COMPILE = arm-linux-
+
 # 保存make指令
-MAKE = make
+		MAKE = make
 # 保存编译命令
-CC      = arm-linux-gcc
+		CC      = $(CROSS_COMPILE)gcc
 # 保存链接命令
-LD      = arm-linux-ld
+		LD      = $(CROSS_COMPILE)ld
 # 保存打包库文件命令
-AR      = arm-linux-ar
+		AR      = $(CROSS_COMPILE)ar
 
 # 保存可执行文件格式转换命令
 # 用来复制一个目标文件的内容到另一个文件中.
 # 此选项可以进行格式的转换.在实际编程的,用的最多的就是
 # 将ELF格式的可执行文件转换为二进制文件
-OBJCOPY = arm-linux-objcopy
+		OBJCOPY = $(CROSS_COMPILE)objcopy
 
 # 保存可执行文件转反汇编代码命令
 #常用来显示二进制文件信息,常用来查看反汇编代码
-OBJDUMP = arm-linux-objdump
+		OBJDUMP = $(CROSS_COMPILE)objdump
+
+		CPP		= $(CC) -E
+		AR		= $(CROSS_COMPILE)ar
+		NM		= $(CROSS_COMPILE)nm
+
+		STRIP		= $(CROSS_COMPILE)strip
 
 # 打包库文件命令参数
-ARFLAGS = crv
-
-# C代码编译 参数
-CFLAGS 		:= -Wall -O2
-# CPP代码编译 参数
-CPPFLAGS   	:= -nostdinc  -fno-builtin #-nostdlibs 
-
-# 链接命令 参数
-LDFLAGS := -Bstatic 
-
-# 链接命令 参数（库文件路径）
-LIBS_PATH := -L /usr/lib
+		ARFLAGS = crv
 
 # 编译参数（头文件路径）
-INC_PATH =
-
+		INC_PATH =
 # 加载 头文件路径变量配置文件
-sinclude $(TOP_DIR)/inc_path.mk
+sinclude $(TOPDIR)/inc_path.mk
 
-# .c -> .o   .S -> .o 生成规则
-%.o:%.c
-	${CC} $(INC_PATH) $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+# C代码编译 参数
 
-%.o:%.S
-	${CC} $(INC_PATH) $(CPPFLAGS) $(CFLAGS) -c -o $@ $<
+		CFLAGS 		:= -Wall -O2 -nostdinc  -fno-builtin  
+		CFLAGS      += $(INC_PATH)
+# CPP代码编译 参数
+		CPPFLAGS   	:= 
+
+# 链接命令 参数
+		LDFLAGS := -Bstatic 
+
+# 链接命令 参数（库文件路径）
+		LIBS_PATH := -L /usr/lib
+
+
+ 
+export MAKE CC LD AR OBJCOPY OBJDUMP CPP AR NM STRIP ARFLAGS CFLAGS CPPFLAGS LDFLAGS LIBS_PATH
+
+endif
+
