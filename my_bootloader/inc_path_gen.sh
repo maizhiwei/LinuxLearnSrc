c04dc5f1f0e6a53a4365d7a365fbda4eab4c426e
maizw <maizw@snovatech.cn>
2020-02-06 06:32:28 +0800

    集成zhiwei修改点： 1. my bootloader 的 inc_path_gen.sh修改bug

--- my_bootloader/inc_path_gen.sh
+++ my_bootloader/inc_path_gen.sh
@@ -2,7 +2,7 @@
 # 删除之前生成的文件
 rm -f inc_path.mk
 # 寻找目录下所有的头文件，提取其绝对路径，并过滤掉重复的路径
-find -name '*.h' | sed 's#^.#'${PWD}'#g' | sed 's#/[^/]*.h##g' | sed 's#^.*$#INC_PATH += -I &#' | sort | uniq > inc_path.mk
+find -name '*.h' | sed 's#^.#'${PWD}'#g' | sed 's#/[^/]*\.h##g' | sed 's#^.*$#INC_PATH += -I &#' | sort | uniq > inc_path.mk
 
 #备用的功能:搜索目录下所有include 或 inc的目录，并转成编译选项
 #find -type d -name 'include' | sed 's#^.#'${PWD}'#g'| sed 's#^.*$#INC_PATH += -I &#' > inc_path.mk
